8dcc7abb4c258da6e8849a472d59152ed73886fa
sleepyjoyy <sleepyjoyy@gmail.com>
2023-02-06 21:55:17 +0900

    Feat: Proxy사용 CORS에러대응

--- api2/server.js
+++ api2/server.js
@@ -1,35 +1,42 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express(),
-      bodyParser = require("body-parser");
-      port = 3070;
+  bodyParser = require("body-parser");
+port = 3070;
 
 // place holder for the data
-const todos = [{
-  todo: "산책하기",
-  category: "운동",
-  isComplete: true
-  }];
+const todos = [
+  {
+    todo: "산책하기",
+    category: "운동",
+    isComplete: true,
+  },
+  {
+    todo: "영화 상견니 보기",
+    category: "여가",
+    isComplete: true,
+  },
+];
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../my-app/build')));
+app.use(express.static(path.join(__dirname, "../my-app/build")));
 
-app.get('/api2/todos', (req, res) => {
-  console.log('api/todos called!')
+app.get("/api2/todos", (req, res) => {
+  console.log("api/todos called!");
   res.json(todos);
 });
 
-app.post('/api2/todo', (req, res) => {
+app.post("/api2/todo", (req, res) => {
   const todo = req.body.todo;
-  console.log('Adding user:::::', todo);
+  console.log("Adding user:::::", todo);
   todos.push(todo);
   res.json("todo added!");
 });
 
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
   res.send(`<h1>API Running on the port ${port}</h1>`);
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the port::${port}`);
 });
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,59 +1,69 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BookTable from './components/BookTable';
-import DisplayBoard from './components/DisplayBoard';
-import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import BookTable from "./components/BookTable";
+import DisplayBoard from "./components/DisplayBoard";
+import CreateBook from "./components/CreateBook";
+import { getAllBooks, getAllTodos, createBook } from "./services/BookService";
+import Footer from "./components/Footer";
+import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 
-function App () {
-
+function App() {
   const [bookShelf, setBookShelf] = useState({});
   const [books, setBooks] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
+  const [numberOfToDos, setNumberTodos] = useState(0);
+  const [todo, setTodos] = useState([]);
 
   const handleSubmit = () => {
-      createBook(bookShelf)
-        .then(() => {
-          setNumberBooks(numberOfBooks+1);
-      });
-  }
+    createBook(bookShelf).then(() => {
+      setNumberBooks(numberOfBooks + 1);
+    });
+  };
 
   const getAllBook = () => {
-    getAllBooks()
-      .then(data => {
-        setBooks(data);
-        setNumberBooks(data.length);
-      });
-  }
+    getAllBooks().then((data) => {
+      setBooks(data);
+      setNumberBooks(data.length);
+    });
+  };
+
+  const getAllTodo = () => {
+    getAllTodos().then((data) => {
+      setTodos(data);
+      setNumberTodos(data.length);
+    });
+  };
 
   const handleOnChangeForm = (e) => {
-      let inputData = bookShelf;
-      if (e.target.name === 'book') {
-        bookShelf.book = e.target.value;
-      } else if (e.target.name === 'category') {
-        bookShelf.category = e.target.value;
-      } else if (e.target.name === 'author') {
-        bookShelf.author = e.target.value;
-      }
-      setBookShelf(inputData);
-  }
+    let inputData = bookShelf;
+    if (e.target.name === "book") {
+      bookShelf.book = e.target.value;
+    } else if (e.target.name === "category") {
+      bookShelf.category = e.target.value;
+    } else if (e.target.name === "author") {
+      bookShelf.author = e.target.value;
+    }
+    setBookShelf(inputData);
+  };
 
-  
   return (
     <div className="main-wrapper">
       <div className="main">
         <Header />
-        <CreateBook 
+        <Todo
+          numberOfToDos={numberOfToDos}
+          getAllTodo={getAllTodo}
+          todo={todo}
+        />
+        <TodoList todo={todo} />
+        <CreateBook
           bookShelf={bookShelf}
           onChangeForm={handleOnChangeForm}
           handleSubmit={handleSubmit}
         />
-        <DisplayBoard 
-          numberOfBooks={numberOfBooks} 
-          getAllBook={getAllBook} 
-        />
+        <DisplayBoard numberOfBooks={numberOfBooks} getAllBook={getAllBook} />
         <BookTable books={books} />
         <Footer />
       </div>
